5ef9c8354e60aaf4dd4a6d0adcbd22fee266b3f5
arolleaguekeng <arolleaguekeng@gmail.com>
2022-12-06 18:47:23 +0100

    pk_modifications

---
+++ bin/objects/tables/constraints/fk/main.sql
@@ -0,0 +1,85 @@
+
+--@constrains.unique
+
+
+
+--@constraints.Foreign Key
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_adresse_besoin_client
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_annonce
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_attribut
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_besoin_client_type_linge
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_besoin_client
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_client
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_adresse
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_offre
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_personne_physique
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_personne
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_pressing_besoin_client
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_pressing
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_promo
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_role
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_service
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_type_linge
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_type_service
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_utilisateur
+
+
+PROMPT******************** add constrains_fk_academic_exp*************************
+@objects/tables/constraints/fk/fk_valeur_attribut
+
+
+
+
+
